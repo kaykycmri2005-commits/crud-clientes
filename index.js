e92ed9880570a15b0ead921c9e1b92b6d82d56ca
KaykyCmri <kaykycmri2005@gmail.com>
2026-03-17 20:47:18 -0300

    Add initial HTML structure for client registration

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Clientes</title>
+</head>
+<body>
+
+    <h1>Clientes</h1>
+
+    <form id="form">
+        <input type="text" id="nome" placeholder="Nome">
+        <input type="email" id="email" placeholder="Email">
+        <button type="submit">Cadastrar</button>
+    </form>
+
+    <ul id="lista"></ul>
+
+    <script type="module" src="./js/app.js"></script>
+</body>
+</html>
